380a2139f7d45511f63721bd1635c7983732a2d9
Antti Kautiainen <antti@kautiainen.com>
2023-11-29 15:04:08 +0200

    fix: emulators not starting
    modified:   .gitignore
    - Remove log files from output.
    
    modified:   firebase.json
    - Removed storage from the client
    
    modified:   functions/index.js
    - Fnctions test index file.
    
    new file:   dune-character-assistant/.eslintrc.js
    new file:   dune-character-assistant/.gitignore
    new file:   dune-character-assistant/index.js
    new file:   dune-character-assistant/package-lock.json
    new file:   dune-character-assistant/package.json
    - Firebase hosting test files.
    
    new file:   public/index.html
    - Deployment test index file.
    
    TODO:

--- dune-character-assistant/index.js
+++ dune-character-assistant/index.js
@@ -13,21 +13,6 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.duneApp = onRequest((request, response) => {
-  const title = "Dune Character Assistant (Testing)";
-  logger.info(title,
-      {structuredData: true});
-  response.send(`<html><head>
-    <title>${title}</title>
-    <style>
-        body {
-            background-color: deeppurple;
-            color: green;
-        }
-    </style>
-    </head><body><h1>${title}</h1></body></html>`);
-});
-
 // exports.helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
