9f7bdaaad0f14c8a533a130a090039b33411edeb
Antti Kautiainen <antti@kautiainen.com>
2023-11-29 10:52:13 +0200

    refactor: changed automatic login to method
    store/admin.mjs:
    - BREAKING CHANGE: The automatic login no longer happens.
    - User should export the adminLogin function to log in.
      - Has default firebase of the main developer

--- store/admin.mjs
+++ store/admin.mjs
@@ -2,7 +2,14 @@ import admin from "firebase-admin";
 
 var serviceAccount = require("../private/serviceAccountKey.json");
 
+/**
+ * Perform admin login to the Firebase.
+ * @param {string} [databaseUrl] The database url of the data storage.
+ * Defaults to the default database url.
+ */
+export function adminLogin(databaseUrl = "https://dune-rpg-assistant-default-rtdb.europe-west1.firebasedatabase.app") {
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://dune-rpg-assistant-default-rtdb.europe-west1.firebasedatabase.app"
+  databaseURL: databaseUrl
 });
+}
